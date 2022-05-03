661ad101659c8c882875c8b57abca351a4e8b535
Gini <djinymail@yahoo.fr>
2022-05-03 16:44:28 +0200

    test cursor

--- dev/js/main.js
+++ dev/js/main.js
@@ -50,7 +50,7 @@ const cursor = document.querySelector('.cursor');
 document.addEventListener('mousemove', (e) => {
 	cursor.setAttribute(
 		'style',
-		'top:' + (e.pageY - 20) + 'px; left:' + (e.pageX - 20) + 'px;'
+		'top:' + (e.pageY) + 'px; left:' + (e.pageX) + 'px;'
 	);
 });
 
