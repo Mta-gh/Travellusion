2acb537f0e63a4a0a51761422c79b98a5369e4d0
Gini <djinymail@yahoo.fr>
2022-05-03 14:38:47 +0200

    test cursor perso

--- dev/js/main.js
+++ dev/js/main.js
@@ -41,3 +41,23 @@ document.addEventListener('scroll', () => {
     theTop = top;
 }
 );
+
+
+//Cursor test//
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', (e) => {
+	cursor.setAttribute(
+		'style',
+		'top:' + (e.pageY - 20) + 'px; left:' + (e.pageX - 20) + 'px;'
+	);
+});
+
+document.addEventListener('click', () => {
+	cursor.classList.add('expand');
+
+	setTimeout(() => {
+		cursor.classList.remove('expand');
+	}, 500);
+});
